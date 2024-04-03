48db1295be633b3ba90404cd1e90716c04f6a3f7
Svyatoslav <pqrupq@gmail.com>
2024-04-03 14:38:37 +0300

    feat(Tooltip): add new component

---
+++ packages/react/src/components/Tooltip/Tooltip.stories.tsx
@@ -0,0 +1,120 @@
+import { ComponentProps, useState } from 'react';
+
+import { Meta, StoryObj } from '@storybook/react';
+
+import { Box } from '@mui/material';
+import Button from '@mui/material/Button';
+
+import { Tooltip } from '.';
+
+type Args = ComponentProps<typeof Tooltip>;
+
+const meta: Meta<Args> = {
+  tags: ['autodocs'],
+  component: Tooltip,
+  parameters: {
+    references: ['Tooltip']
+  },
+  argTypes: {
+    title: {
+      table: {
+        disable: true
+      }
+    },
+    children: {
+      table: {
+        disable: true
+      }
+    },
+    components: {
+      table: {
+        disable: true
+      }
+    },
+    componentsProps: {
+      table: {
+        disable: true
+      }
+    },
+    describeChild: {
+      table: {
+        disable: true
+      }
+    },
+    id: {
+      table: {
+        disable: true
+      }
+    },
+    PopperComponent: {
+      table: {
+        disable: true
+      }
+    },
+    PopperProps: {
+      table: {
+        disable: true
+      }
+    },
+    slots: {
+      table: {
+        disable: true
+      }
+    },
+    slotProps: {
+      table: {
+        disable: true
+      }
+    },
+    TransitionComponent: {
+      table: {
+        disable: true
+      }
+    },
+    TransitionProps: {
+      table: {
+        disable: true
+      }
+    }
+  }
+};
+
+export default meta;
+type Story = StoryObj<Args>;
+
+export const Demo: Story = {
+  render: function Render(args, { globals: { locale } }) {
+    const isCustomColor = args.color === 'monoB' || args.color === 'monoB.A600' || args.color === 'white.A600';
+
+    const [isOpen, setOpen] = useState(false);
+
+    const handleToggle = () => {
+      setOpen(!isOpen);
+    };
+
+    return (
+      <Box
+        sx={(theme) => ({
+          width: '100%',
+          height: 200,
+          backgroundColor: isCustomColor ? theme.palette.monoA.A100 : theme.palette.surface.background,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center'
+        })}
+      >
+        <Tooltip
+          {...args}
+          open={args.open === undefined ? isOpen : args.open}
+          title={locale === 'en' ? 'Tooltip' : 'Подсказка'}
+          onClose={() => setOpen(false)}
+          onOpen={() => setOpen(true)}
+        >
+          <Button variant="contained" onClick={handleToggle}>
+            {locale === 'en' ? 'Button' : 'Кнопка'}
+          </Button>
+        </Tooltip>
+      </Box>
+    );
+  }
+};
---
+++ packages/react/src/components/Tooltip/Tooltip.types.ts
@@ -0,0 +1,219 @@
+import { ElementType, HTMLAttributes, HTMLProps, JSXElementConstructor, ReactNode, SyntheticEvent } from 'react';
+
+import { TooltipClasses } from './Tooltip.classes';
+
+import { MUIStyledCommonProps, SxProps } from '@mui/system';
+import { InternalStandardProps as StandardProps, Theme } from '@mui/material';
+import { PopperProps } from '@mui/material/Popper';
+import { TransitionProps } from '@mui/material/transitions';
+
+export type DefaultColors = 'primary' | 'secondary' | 'error' | 'warning' | 'info' | 'success' | 'error';
+
+export type CustomColors = 'monoA.A600' | 'monoB' | 'monoB.A600' | 'white.A600';
+
+export interface TooltipComponentsPropsOverrides {}
+
+export interface TooltipProps extends StandardProps<HTMLAttributes<HTMLDivElement>, 'title'> {
+  /**
+   * If `true`, adds an arrow to the tooltip.
+   * @default true
+   */
+  arrow?: boolean;
+  /**
+   * Tooltip reference element.
+   */
+  children: ReactNode;
+  /**
+   * Override or extend the styles applied to the component.
+   */
+  classes?: Partial<TooltipClasses>;
+  /** Class applied to the root element. */
+  className?: string;
+  /**
+   * The color of the component.
+   * @default 'monoA.A600'
+   */
+  color?: DefaultColors | CustomColors;
+  /**
+   * The components used for each slot inside.
+   *
+   * This prop is an alias for the `slots` prop.
+   * It's recommended to use the `slots` prop instead.
+   *
+   * @default {}
+   */
+  components?: {
+    Popper?: ElementType<PopperProps>;
+    Transition?: ElementType;
+    Tooltip?: ElementType;
+    Arrow?: ElementType;
+  };
+  /**
+   * The extra props for the slot components.
+   * You can override the existing props or add new ones.
+   *
+   * This prop is an alias for the `slotProps` prop.
+   * It's recommended to use the `slotProps` prop instead, as `componentsProps` will be deprecated in the future.
+   *
+   * @default {}
+   */
+  componentsProps?: {
+    popper?: Partial<PopperProps> & TooltipComponentsPropsOverrides;
+    transition?: TransitionProps & TooltipComponentsPropsOverrides;
+    tooltip?: HTMLProps<HTMLDivElement> & MUIStyledCommonProps & TooltipComponentsPropsOverrides;
+    arrow?: HTMLProps<HTMLSpanElement> & MUIStyledCommonProps & TooltipComponentsPropsOverrides;
+  };
+  /**
+   * Set to `true` if the `title` acts as an accessible description.
+   * By default the `title` acts as an accessible label for the child.
+   * @default false
+   */
+  describeChild?: boolean;
+  /**
+   * Do not respond to focus-visible events.
+   * @default false
+   */
+  disableFocusListener?: boolean;
+  /**
+   * Do not respond to hover events.
+   * @default false
+   */
+  disableHoverListener?: boolean;
+  /**
+   * Makes a tooltip not interactive, i.e. it will close when the user
+   * hovers over the tooltip before the `leaveDelay` is expired.
+   * @default false
+   */
+  disableInteractive?: boolean;
+  /**
+   * Do not respond to long press touch events.
+   * @default false
+   */
+  disableTouchListener?: boolean;
+  /**
+   * The number of milliseconds to wait before showing the tooltip.
+   * This prop won't impact the enter touch delay (`enterTouchDelay`).
+   * @default 100
+   */
+  enterDelay?: number;
+  /**
+   * The number of milliseconds to wait before showing the tooltip when one was already recently opened.
+   * @default 0
+   */
+  enterNextDelay?: number;
+  /**
+   * The number of milliseconds a user must touch the element before showing the tooltip.
+   * @default 700
+   */
+  enterTouchDelay?: number;
+  /**
+   * If `true`, the tooltip follow the cursor over the wrapped element.
+   * @default false
+   */
+  followCursor?: boolean;
+  /**
+   * This prop is used to help implement the accessibility logic.
+   * If you don't provide this prop. It falls back to a randomly generated id.
+   */
+  id?: string;
+  /**
+   * The number of milliseconds to wait before hiding the tooltip.
+   * This prop won't impact the leave touch delay (`leaveTouchDelay`).
+   * @default 0
+   */
+  leaveDelay?: number;
+  /**
+   * The number of milliseconds after the user stops touching an element before hiding the tooltip.
+   * @default 1500
+   */
+  leaveTouchDelay?: number;
+  /**
+   * Callback fired when the component requests to be closed.
+   *
+   * @param {SyntheticEvent} event The event source of the callback.
+   */
+  onClose?: (event: SyntheticEvent | Event) => void;
+  /**
+   * Callback fired when the component requests to be open.
+   *
+   * @param {SyntheticEvent} event The event source of the callback.
+   */
+  onOpen?: (event: SyntheticEvent) => void;
+  /**
+   * If `true`, the component is shown.
+   */
+  open?: boolean;
+  /**
+   * Tooltip placement.
+   * @default 'bottom'
+   */
+  placement?:
+    | 'bottom-end'
+    | 'bottom-start'
+    | 'bottom'
+    | 'left-end'
+    | 'left-start'
+    | 'left'
+    | 'right-end'
+    | 'right-start'
+    | 'right'
+    | 'top-end'
+    | 'top-start'
+    | 'top';
+  /**
+   * The component used for the popper.
+   * @default Popper
+   */
+  PopperComponent?: JSXElementConstructor<PopperProps>;
+  /**
+   * Props applied to the [`Popper`](/material-ui/api/popper/) element.
+   * @default {}
+   */
+  PopperProps?: Partial<PopperProps>;
+  /**
+   * The extra props for the slot components.
+   * You can override the existing props or add new ones.
+   *
+   * This prop is an alias for the `componentsProps` prop, which will be deprecated in the future.
+   *
+   * @default {}
+   */
+  slotProps?: {
+    popper?: Partial<PopperProps> & TooltipComponentsPropsOverrides;
+    transition?: TransitionProps & TooltipComponentsPropsOverrides;
+    tooltip?: HTMLProps<HTMLDivElement> & MUIStyledCommonProps<Theme> & TooltipComponentsPropsOverrides;
+    arrow?: HTMLProps<HTMLSpanElement> & MUIStyledCommonProps<Theme> & TooltipComponentsPropsOverrides;
+  };
+  /**
+   * The components used for each slot inside.
+   *
+   * This prop is an alias for the `components` prop, which will be deprecated in the future.
+   *
+   * @default {}
+   */
+  slots?: {
+    popper?: ElementType<PopperProps>;
+    transition?: ElementType;
+    tooltip?: ElementType;
+    arrow?: ElementType;
+  };
+  /**
+   * The system prop that allows defining system overrides as well as additional CSS styles.
+   */
+  sx?: SxProps<Theme>;
+  /**
+   * Tooltip title. Zero-length titles string, undefined, null and false are never displayed.
+   */
+  title: ReactNode;
+  /**
+   * The component used for the transition.
+   * [Follow this guide](/material-ui/transitions/#transitioncomponent-prop) to learn more about the requirements for this component.
+   * @default Fade
+   */
+  TransitionComponent?: JSXElementConstructor<TransitionProps & { children: ReactNode }>;
+  /**
+   * Props applied to the transition element.
+   * By default, the element is based on this [`Transition`](https://reactcommunity.org/react-transition-group/transition/) component.
+   */
+  TransitionProps?: TransitionProps;
+}
