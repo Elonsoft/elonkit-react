37ab40ccbb390876e89b3c26d82583cb75718dcf
Denis Yakshov <feerzlay@gmail.com>
2024-05-08 12:12:31 +0300

    style: upgrade eslint to v9

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,1 +1,1 @@
-module.exports = require('@elonkit/prettier-config');
+export { default } from '@elonkit/prettier-config';
--- packages/react/src/components/Spinner/SpinnerFadingDoubleRing/SpinnerFadingDoubleRing.types.ts
+++ packages/react/src/components/Spinner/SpinnerFadingDoubleRing/SpinnerFadingDoubleRing.types.ts
@@ -2,5 +2,4 @@ import { SpinnerBaseProps } from '../Spinner.types';
 
 import { SpinnerFadingDoubleRingClasses } from './SpinnerFadingDoubleRing.classes';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface SpinnerFadingDoubleRingProps extends SpinnerBaseProps<SpinnerFadingDoubleRingClasses> {}
